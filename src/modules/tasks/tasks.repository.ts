736ef328910d2d7b98d1a12b7b25858c3d04841a
Abilov599 <abilovv599@gmail.com>
2025-01-18 18:45:40 +0400

    fix query

--- src/modules/tasks/tasks.repository.ts
+++ src/modules/tasks/tasks.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource, Like, Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Task } from './entities/task.entity';
 import { GetFilteredTasksDto } from './dto/get-filtered-tasks.dto';
 
@@ -9,41 +9,41 @@ export class TasksRepository extends Repository<Task> {
     super(Task, dataSource.createEntityManager());
   }
 
-  // public getTasks(filterDto: GetFilteredTasksDto) {
-  //   const { status, search } = filterDto;
-  //
-  //   const query = this.createQueryBuilder('tasks');
-  //
-  //   if (status) {
-  //     query.andWhere('tasks.status = :status', { status });
-  //   }
-  //
-  //   if (search) {
-  //     query.andWhere(
-  //       'LOWER(tasks.title) LIKE LOWER(:search) OR LOWER(tasks.description) LIKE LOWER(:search)',
-  //       { search: `%${search}%` },
-  //     );
-  //   }
-  //
-  //   return query.getMany();
-  // }
-
-  public async getTasks(filterDto: GetFilteredTasksDto) {
+  public getTasks(filterDto: GetFilteredTasksDto) {
     const { status, search } = filterDto;
 
-    const where: any = {};
+    const query = this.createQueryBuilder('tasks');
 
     if (status) {
-      where.status = status;
+      query.andWhere('tasks.status = :status', { status });
     }
 
     if (search) {
-      where.title = Like(`%${search}%`);
-      where.description = Like(`%${search}%`);
+      query.andWhere(
+        '(LOWER(tasks.title) LIKE LOWER(:search) OR LOWER(tasks.description) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
     }
 
-    return this.find({ where });
+    return query.getMany();
   }
+
+  // public async getTasks(filterDto: GetFilteredTasksDto) {
+  //   const { status, search } = filterDto;
+  //
+  //   const where: any = {};
+  //
+  //   if (status) {
+  //     where.status = status;
+  //   }
+  //
+  //   if (search) {
+  //     where.title = Like(`%${search}%`);
+  //     where.description = Like(`%${search}%`);
+  //   }
+  //
+  //   return this.find({ where });
+  // }
 
   public async deleteTask(id: string) {
     const result = await this.delete(id);
