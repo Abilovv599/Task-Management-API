99e69af16012dc839b1b907c700b7d2364b57866
abilovv599 <abilovv599@gmail.com>
2025-02-05 13:14:48 +0400

    add swagger

--- src/modules/swagger/swagger.builder.ts
+++ src/modules/swagger/swagger.builder.ts
@@ -1,12 +1,22 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
 export function createSwagger(app: NestExpressApplication) {
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Task Management API')
+    .setDescription('The Task Management API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'access-token',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // This name will be used as the name of the security scheme
+    )
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
